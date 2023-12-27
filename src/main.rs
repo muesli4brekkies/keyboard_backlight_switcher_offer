0d4103ad4d10e96781467bb3012b5d22b643810a
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-27 21:21:30 +0000

    big refactor

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,33 @@
-extern crate device_query;
-
-use device_query::{DeviceEvents, DeviceState, Keycode};
+use device_query::{DeviceEvents, DeviceState};
 use std::{
   fs::{read_to_string, write},
   io::Result,
-  thread::sleep,
-  time::Duration,
+  sync::{Arc, Mutex},
 };
 
-const TIMEOUT: &str = "6";
-const TICK: u64 = 3;
-const RUN_FILE: &str = "/home/muesli/.kbblso";
+const TIMEOUT: u64 = 6;
 const BRIGHTNESS_PATH: &str = "/sys/class/leds/asus::kbd_backlight/brightness";
 const BRIGHTNESS_SETTING_PATH: &str = "/sys/class/leds/asus::kbd_backlight/brightness_hw_changed";
 
-fn callback(_: &Keycode) {
-  (|| -> Result<()> {
-    write(RUN_FILE, TIMEOUT)?;
-    write(BRIGHTNESS_PATH, read_to_string(BRIGHTNESS_SETTING_PATH)?)?;
-    Ok(())
-  })()
-  .expect("Failed to write to file")
-}
-
-fn switch() -> Result<()> {
-  let timer = std::fs::read_to_string(RUN_FILE)?
-    .parse::<u64>()
-    .unwrap_or(0);
-  match timer {
-    0 => write(BRIGHTNESS_PATH, "0")?,
-    _ => write(RUN_FILE, (timer - TICK).to_string())?,
-  }
-  sleep(Duration::from_secs(TICK));
-  Ok(())
-}
-
 fn main() -> Result<()> {
-  write(RUN_FILE, TIMEOUT)?;
-  let _callback_guard = DeviceState::new().on_key_down(callback);
+  let time_mtx = Arc::new(Mutex::new(std::time::SystemTime::now()));
+  let time_cpy = Arc::clone(&time_mtx);
+  let _callback_guard = DeviceState::new().on_key_down(move |_| {
+    (|| -> Result<()> {
+      let mut time = time_cpy.lock().unwrap();
+      *time = std::time::SystemTime::now();
+      write(BRIGHTNESS_PATH, read_to_string(BRIGHTNESS_SETTING_PATH)?)?;
+      Ok(())
+    })()
+    .expect("I/O fail!")
+  });
   loop {
-    switch()?
+    if std::time::SystemTime::elapsed(&Arc::clone(&time_mtx).lock().unwrap())
+      .unwrap()
+      .as_secs()
+      > TIMEOUT
+    {
+      write(BRIGHTNESS_PATH, "0")?
+    }
   }
 }
