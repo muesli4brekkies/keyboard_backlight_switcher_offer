f495f78aee55885904385f7e173c4be33cb374a4
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-27 23:21:33 +0000

    big refactor

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,53 @@
 use device_query::{DeviceEvents, DeviceState};
 use std::{
+  env,
   fs::{read_to_string, write},
   io::Result,
   sync::{Arc, Mutex},
+  thread::sleep,
+  time::{Duration, SystemTime},
 };
 
 const TIMEOUT: u64 = 6;
+const TICK: u64 = 3;
 const BRIGHTNESS_PATH: &str = "/sys/class/leds/asus::kbd_backlight/brightness";
 const BRIGHTNESS_SETTING_PATH: &str = "/sys/class/leds/asus::kbd_backlight/brightness_hw_changed";
 
+fn get_brightness_path() -> String {
+  env::args()
+    .enumerate()
+    .fold(None, |a, (i, arg)| match arg == "-p" {
+      true => match env::args().nth(i + 1) {
+        Some(r) => Some(r),
+        None => a,
+      },
+      false => a,
+    })
+    .unwrap_or(String::from(BRIGHTNESS_PATH))
+}
+
 fn main() -> Result<()> {
-  let time_mtx = Arc::new(Mutex::new(std::time::SystemTime::now()));
+  let write_brightness =
+    |contents: String| -> Result<()> { write(get_brightness_path(), contents) };
+  let time_mtx = Arc::new(Mutex::new(SystemTime::now()));
   let time_cpy = Arc::clone(&time_mtx);
   let _callback_guard = DeviceState::new().on_key_down(move |_| {
     (|| -> Result<()> {
       let mut time = time_cpy.lock().unwrap();
-      *time = std::time::SystemTime::now();
-      write(BRIGHTNESS_PATH, read_to_string(BRIGHTNESS_SETTING_PATH)?)?;
+      *time = SystemTime::now();
+      write_brightness(read_to_string(BRIGHTNESS_SETTING_PATH)?)?;
       Ok(())
     })()
     .expect("I/O fail!")
   });
   loop {
-    if std::time::SystemTime::elapsed(&Arc::clone(&time_mtx).lock().unwrap())
+    if SystemTime::elapsed(&Arc::clone(&time_mtx).lock().unwrap())
       .unwrap()
       .as_secs()
       > TIMEOUT
     {
-      write(BRIGHTNESS_PATH, "0")?
+      write_brightness(String::from("0"))?
     }
+    sleep(Duration::from_secs(TICK));
   }
 }
