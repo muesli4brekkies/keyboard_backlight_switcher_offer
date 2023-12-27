679b2c7633ef112535f921c74c1384e68cc808d3
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-27 16:56:32 +0000

    tidy up

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use device_query::{DeviceEvents, DeviceState};
 use std::thread;
 use std::time::Duration;
 
-const RUN_FILE: &'static str = "./kbblso.tim";
+const RUN_FILE: &'static str = "/home/muesli/.kbblso";
 const BRIGHTNESS_PATH: &'static str = "/sys/class/leds/asus::kbd_backlight/brightness";
 const BRIGHTNESS_SETTING_PATH: &'static str =
   "/sys/class/leds/asus::kbd_backlight/brightness_hw_changed";
@@ -13,7 +13,7 @@ fn main() {
   std::fs::File::create(RUN_FILE).expect("Failed to create file :( ");
   std::fs::write(RUN_FILE, "5000").unwrap_or(());
   let device_state = DeviceState::new();
-  let _ = device_state.on_key_down(|_| {
+  let _guard = device_state.on_key_down(|_| {
     std::fs::write(RUN_FILE, "5000").unwrap_or(());
     std::fs::write(
       BRIGHTNESS_PATH,
