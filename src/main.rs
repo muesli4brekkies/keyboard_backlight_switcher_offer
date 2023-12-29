ec6635785b47167f925caf8608f0b8e0cd78007b
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-29 02:50:06 +0000

    adjustments

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,52 @@
 use std::{
   fs::{read_to_string, write},
-  os::raw::c_char,
   ptr, slice,
   thread::sleep,
   time::{Duration, SystemTime},
 };
 use x11::xlib::{self, _XDisplay};
 
+const PATH: &str = "/sys/class/leds/asus::kbd_backlight";
 fn write_brightness(turn_off: bool) {
-  let path = "/sys/class/leds/asus::kbd_backlight";
   write(
-    format!("{path}/brightness"),
+    format!("{PATH}/brightness"),
     match turn_off {
       true => String::from("0"),
-      false => read_to_string(format!("{path}/brightness_hw_changed")).unwrap(),
+      false => read_to_string(format!("{PATH}/brightness_hw_changed")).unwrap_or(String::from("0")),
     },
   )
   .unwrap()
 }
 
 fn check_keys(display: *mut _XDisplay) -> bool {
-  let keymap: *mut c_char = [0; 32].as_mut_ptr();
+  let keymap = [0; 32].as_mut_ptr();
   unsafe {
     xlib::XQueryKeymap(display, keymap);
     slice::from_raw_parts(keymap, 32)
-      .iter()
-      .fold(false, |a, b| if *b == 0 { a } else { true })
   }
+  .iter()
+  .fold(false, |a, b| if *b == 0 { a } else { true })
 }
 
+fn get_display() -> *mut _XDisplay {
+  let display = unsafe { xlib::XOpenDisplay(ptr::null()) };
+  match unsafe { display.as_ref().is_some() } {
+    true => display,
+    false => panic!("Could not connect to a X display"),
+  }
+}
 fn main() {
-  unsafe {
-    let display = xlib::XOpenDisplay(ptr::null());
-    if display.as_ref().is_none() {
-      panic!("Could not connect to a X display");
+  let display = get_display();
+  let mut last_keypress_time = SystemTime::now();
+  loop {
+    dbg!(last_keypress_time);
+    if check_keys(display) {
+      last_keypress_time = SystemTime::now();
+      write_brightness(false)
     }
-    let mut last_keypress_time = SystemTime::now();
-    loop {
-      if check_keys(display) {
-        last_keypress_time = SystemTime::now();
-        write_brightness(false)
-      }
-      if last_keypress_time.elapsed().unwrap() > Duration::from_secs(5) {
-        write_brightness(true)
-      }
-      sleep(Duration::from_millis(500));
+    if last_keypress_time.elapsed().unwrap() > Duration::from_secs(5) {
+      write_brightness(true)
     }
+    sleep(Duration::from_millis(500));
   }
 }
